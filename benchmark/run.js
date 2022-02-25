2d4b1e6df1bb7ec415791f5860ae90f5cb908a85
shufo <meikyowise@gmail.com>
2022-02-26 02:44:20 +0900

    perf: ⚡️ add benchmark script to measure performance

---
+++ benchmark/run.js
@@ -0,0 +1,21 @@
+const b = require('benny');
+const { sortClasses } = require('../');
+
+b.suite(
+  'sort benchmark',
+
+  b.add('sort 2 classes', () => {
+    sortClasses('pt-4 p-2');
+  }),
+
+  b.add('sort many classes', () => {
+    sortClasses(
+      'prose lg:prose-xl foo container z-10 z-20 z-50 justify-center text-left md:text-centerddk'
+    );
+  }),
+
+  b.cycle(),
+  b.complete(),
+  b.save({ file: 'reduce', version: '1.0.0' }),
+  b.save({ file: 'reduce', format: 'chart.html' })
+);
